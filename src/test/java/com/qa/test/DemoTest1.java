534bd351a3765ae5965a1021f29bf7cfb66b5b3e
kajamugan <kajablver@gmail.com>
2021-08-02 17:07:51 +0530

    init2 commit

---
+++ src/test/java/com/qa/test/DemoTest1.java
@@ -0,0 +1,41 @@
+package com.qa.test;
+
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest1 {
+	 @Test
+	  public void sum() {
+	  System.out.println("SUM");
+	  int a = 10;
+	  int b = 20;
+	  Assert.assertEquals(30, a+b);
+	  }
+	  
+	  @Test
+	  public void sub() {
+	  System.out.println("SUBSTRACTION");
+	  int a = 10;
+	  int b = 20;
+	  Assert.assertEquals(10, b-a);
+	  }
+
+
+	  @Test
+	  public void div() {
+	  System.out.println("DIVISION");
+	  int a = 10;
+	  int b = 20;
+	  Assert.assertEquals(2, b/a);
+	  }
+
+
+	  @Test
+	  public void multi() {
+	  System.out.println("MULTIPLICATION");
+	  int a = 10;
+	  int b = 20;
+	  Assert.assertEquals(200, a*b);
+	  }
+
+}
